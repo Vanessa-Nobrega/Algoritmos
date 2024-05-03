b71e238d3eaa43a40802ed4fd5f2ae26272348e1
Vanessa-Nobrega <98487482+Vanessa-Nobrega@users.noreply.github.com>
2024-05-03 19:14:33 -0300

    Add files via upload

---
+++ aula11/aula11/exercício02.py
@@ -0,0 +1,8 @@
+filmes = {
+    'avatar': 2009,
+    'titanic': 1997,
+    'starwars': 2015,
+    'harry-potter': 2011,
+    'avengers': 2012
+}
+print(filmes.get('avatar'))
---
+++ aula11/aula11/exercício03.py
@@ -0,0 +1,18 @@
+filmes = {
+    'avatar': 2009,
+    'titanic': 1997,
+    'starwars': 2015,
+    'harry-potter': 2011,
+    'avengers': 2012
+}
+#items
+#print(filmes.items())
+for chave, valor in filmes.items():
+    print(f"Assisti a {chave.upper()} lançado em {valor}!")
+#keys
+#print(filmes.keys())
+
+#values
+#print(filmes.values())
+
+
